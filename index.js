99b8989b58a1114bbde56869bd6c9c3574bde455
dev <yassinej@gmail.com>
2017-08-01 22:33:28 +0300

    adding OAUTH

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accesToken => console.log(accesToken)
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
 app.listen(PORT);
