6e64f2f5e88597afbee8a40e67df9939ec67115b
dev <yassinej@gmail.com>
2017-08-03 10:47:37 +0300

    adding proxy

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accesToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
